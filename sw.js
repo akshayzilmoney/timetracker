f4404b22fa53d440cecf7f950e29ec14b51062f0
Akshay@ZilMoney.com <akshay@zilmoney.com>
2026-08-10 18:46:47 +0530

    Day editor, holiday/sick days, Notepad, Paint, Task Manager, icon drag + cut/paste, working Display Properties

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Simple offline-first service worker for the Office Time Tracker PWA
-const CACHE = 'timetracker-v11';
+const CACHE = 'timetracker-v12';
 const ASSETS = ['./', 'index.html', 'manifest.json', 'icon.svg'];
 
 self.addEventListener('install', e => {
