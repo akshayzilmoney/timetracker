2a4250b5426b00a5ab8608ab8930b68dbeb7f0cf
Akshay@ZilMoney.com <akshay@zilmoney.com>
2026-08-10 17:30:17 +0530

    Add local-only PIN protection for profile logins

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Simple offline-first service worker for the Office Time Tracker PWA
-const CACHE = 'timetracker-v6';
+const CACHE = 'timetracker-v7';
 const ASSETS = ['./', 'index.html', 'manifest.json', 'icon.svg'];
 
 self.addEventListener('install', e => {
