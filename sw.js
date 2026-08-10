ac565c4d59c8d40f647ddd8db1d171f1a1308299
Akshay@ZilMoney.com <akshay@zilmoney.com>
2026-08-10 17:45:04 +0530

    Add User Setup window (backup-first password flow) and connect-via-backup login

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Simple offline-first service worker for the Office Time Tracker PWA
-const CACHE = 'timetracker-v7';
+const CACHE = 'timetracker-v8';
 const ASSETS = ['./', 'index.html', 'manifest.json', 'icon.svg'];
 
 self.addEventListener('install', e => {
