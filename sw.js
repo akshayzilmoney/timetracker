d0fcc498460f35090d46a68699bd91e48963cd41
Akshay@ZilMoney.com <akshay@zilmoney.com>
2026-08-10 18:02:39 +0530

    Remove default user; login starts empty until a user is created

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Simple offline-first service worker for the Office Time Tracker PWA
-const CACHE = 'timetracker-v9';
+const CACHE = 'timetracker-v10';
 const ASSETS = ['./', 'index.html', 'manifest.json', 'icon.svg'];
 
 self.addEventListener('install', e => {
