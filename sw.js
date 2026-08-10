a31e1ff21fb4a38de9ece5df617474568264c7d0
Akshay@ZilMoney.com <akshay@zilmoney.com>
2026-08-10 18:54:00 +0530

    Widen default windowed size so content fits without scrollbars

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Simple offline-first service worker for the Office Time Tracker PWA
-const CACHE = 'timetracker-v12';
+const CACHE = 'timetracker-v13';
 const ASSETS = ['./', 'index.html', 'manifest.json', 'icon.svg'];
 
 self.addEventListener('install', e => {
