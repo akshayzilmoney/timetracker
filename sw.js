91f7d5ce5da058c673e39bf4b581e7a1cba856b3
Akshay@ZilMoney.com <akshay@zilmoney.com>
2026-08-17 15:01:39 +0530

    Remove the manual Punch Out Now button
    Punches come from the ATS now; a local fake punch-out only created data
    the next sync would overwrite.

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Simple offline-first service worker for the Office Time Tracker PWA
-const CACHE = 'timetracker-v29';
+const CACHE = 'timetracker-v30';
 const ASSETS = ['./', 'index.html', 'manifest.json', 'icon.svg'];
 
 self.addEventListener('install', e => {
